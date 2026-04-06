163a30176c5ba109a92ccf55f099d023fd001333
ArthurCTLin <arthur830119@gmail.com>
2026-04-07 00:21:06 +0800

    FIX: set base path for github pages styling

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/personal_website/'
   title: "AEGIS Lab",
   description: "Arthur's Advanced AI Solutions",
   themeConfig: {
